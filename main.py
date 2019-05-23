34982f8af641c971803ca9fd2157ae5a47e47a2d
FAmorris <fjmorris@sina.com>
2019-05-23 20:56:08 +0800

    20190523205600

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-from dsmodels import security
-import pprint
+#! /root/apps/python3/bin
+
+from dsmodels.security import VaporCloudExplosion, PoolFire, PointSourceGasDiffusion
+
 import flask
+import flask_script
+import pandas as pd
+import json
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'hello, world!'
+
+
+@app.route('/dsmodels/v1.0.0/security/vce/radius', method=['POST'])
+def get_vce_radius():
+    data = request.json['data']
+    
+    return res
 
 if '__main__' == __name__:
-    pprint.pprint([name for name in dir(security) if not name.startswith('_')])
+    app_manager = flask_sript.Manager(app)
+    app_manager.run()
