e7330c6bc38c466b26dfd35333cccea949aca6c8
FAmorris <fjmorris@sina.com>
2019-05-23 23:06:51 +0800

    20190523230600

--- main.py
+++ main.py
@@ -18,9 +18,24 @@ def index():
 @app.route('/dsmodels/v1.0.0/security/vce/radius', methods=['POST'])
 def get_vce_radius():
     data = request.json['data']
+    material = data['material']
+    mat_params = data['mat_params']
+    env_params = data['env_params']
+    ops = data['ops']
+    
+    res = {}
+    
+    try:
+        vce = VaporCloudExplosion(material, mat_params, env_params)
+        radiuses = [vce.calc_wave_radius(op, env_params['eta'], env_params['theta']) for op in ops]
+        res['Code'] = 0
+        res['Message'] = 'Success'
+        res['data'] = radiuses
+        res['result']
+    except:
     
     return res
 
 if '__main__' == __name__:
-    app_manager = flask_sript.Manager(app)
+    app_manager = flask_script.Manager(app)
     app_manager.run()
