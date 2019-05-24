7e1a5573e49cf8cc2b7155d49416a83cb180a5e3
FAmorris <fjmorris@sina.com>
2019-05-24 13:24:04 +0800

    20190524132300

--- main.py
+++ main.py
@@ -15,28 +15,59 @@ def index():
     return 'hello, world!'
 
 
-@app.route('/dsmodels/v1.0.0/security/vce/radius', methods=['POST'])
+@app.route('/tad/v1.0.0/dsmodels/security/api/hurtScope', methods=['POST'])
 def get_vce_radius():
     data = request.json['data']
+    modeltype = data['modeltype']
     material = data['material']
     mat_params = pd.Series(data['mat_params'])
     env_params = pd.Series(data['env_params'])
-    ops = data['ops']
+    fparams = data['fparams']
+    modelins = func_params['modelins']
     
     try:
-        vce = VaporCloudExplosion(material, mat_params, env_params)
-        radiuses = [vce.calc_wave_radius(op, env_params['alpha'], env_params['beta']) for op in ops]
+        if 'VaporCloudExplosion' == modeltype:
+            vce = VaporCloudExplosion(material, mat_params, env_params)
+            modelouts = [vce.calc_wave_radius(modelin, fparams['alpha'], fparams['beta']) for modelin in modelins]
+        elif 'PoolFire' == modeltype:
+            pf = PoolFire(material, mat_params, env_params)
+            modelouts = [pf.calc_heat_radiation(modelin, fparams['eta'], fparams['theta']) for modelin in modelins]
+        elif 'PointSourceGasDiffusion' == modeltype:
+            psgd = PointSourceGasDiffusion(material, mat_params, env_params)
+            modelouts = [psgd.calc_distribution(modelin, fparams['t'], fparams['ddis'], fparams['srch'], fparams['step'])]
+            
         res_code = 0
-        res_msg = 'Success'
-        res_data = radiuses
+        res_msg = 'success'
+        res_data = {'material': material, 'modelouts': modelins}
     except:
         res_code = 1
-        res_msg = 'Faild'
+        res_msg = 'faild'
         res_data = None
         
-    res= {'code': res_code, 'massege': res_msg, 'data': res_data}
-    return json.dumps(res)
+    return json.dumps({'code': res_code, 'massege': res_msg, 'data': res_data})
+    
+    
 
 if '__main__' == __name__:
     app_manager = flask_script.Manager(app)
     app_manager.run()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
